d8811367f24fb186c4884e622fac4f5e0612fb6e
POPs <148493288+probabilityofprofit@users.noreply.github.com>
2023-10-19 18:26:42 -0700

    Update newmaincolor.py

--- pop/newmaincolor.py
+++ pop/newmaincolor.py
@@ -4,9 +4,7 @@ import numpy as np
 import multiprocessing
 import poptions
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
-from numba import jit
 
 # Define the style to hide Streamlit elements
 hide_streamlit_style = """

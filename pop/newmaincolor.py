9cbe4b27d9010dd78e18822cb788f075852ee39b
POPs <148493288+probabilityofprofit@users.noreply.github.com>
2023-10-22 15:30:10 -0700

    Update newmaincolor.py

--- pop/newmaincolor.py
+++ pop/newmaincolor.py
@@ -197,9 +197,9 @@ def main():
             # Display the calculated values
             st.write(f"Entry Cost: ${entry_cost:.2f}")
             st.write(f"Maximum Return: ${max_profit:.2f}")
+            st.write(f"Maximum Return on Risk: {max_return_on_risk * 100:.2f}%")
             st.write(f"Arithmetic-Mean POP: {mean_pop:.2f}%")
             st.write(f"Geometric-Mean POP: {geometric_mean_pop * 100:.2f}%")
-            st.write(f"Maximum Return on Risk: {max_return_on_risk * 100:.2f}%")
 
     except Exception as e:
         st.error(f"An error occurred: {e}")

994f26042def96893b53b839aa33d3163bf64581
POPs <148493288+probabilityofprofit@users.noreply.github.com>
2023-10-25 12:22:38 -0700

    Update SLCallCreditSpread.py

--- pop/SLCallCreditSpread.py
+++ pop/SLCallCreditSpread.py
@@ -91,9 +91,9 @@ def custom_pop_colormap():
     return LinearSegmentedColormap.from_list('custom_pop_colormap', colors)
 
 # Function to calculate the probability to breakeven (POPBE)
-def calculate_popbe(percentage_to_cover_entry_cost, percentage_array, pop_results):
-    # Round the percentage to cover entry cost to the nearest integer
-    rounded_percentage = round(percentage_to_cover_entry_cost)
+def calculate_popbe(percentage_to_cover_entry_credit, percentage_array, pop_results):
+    # Round the percentage to cover Entry Credit to the nearest integer
+    rounded_percentage = round(percentage_to_cover_entry_credit)
 
     # Get the last day of days_to_expiration
     last_day = max(pop_results.columns)
@@ -191,10 +191,10 @@ def main():
             plt.tight_layout()
             st.pyplot(plt)
 
-            # Calculate the entry cost for the put credit spread
-            entry_cost = (short_price - long_price)*100
+            # Calculate the Entry Credit for the put credit spread
+            entry_credit = (short_price - long_price)*100
             
-            # Calculate the entry cost for the call credit spread
+            # Calculate the Entry Credit for the call credit spread
             max_risk = ((long_strike - short_strike) + (long_price - short_price))*100
 
             # Calculate and display the maximum profit
@@ -203,8 +203,8 @@ def main():
             # Calculate the maximum return on risk for call credit spreads
             max_return_on_risk = max_profit / max_risk
 
-            # Calculate the percentage on the maximum return to make entry cost back
-            percentage_to_cover_entry_cost = (entry_cost / max_profit) * 100
+            # Calculate the percentage on the maximum return to make Entry Credit back
+            percentage_to_cover_entry_credit = (entry_credit / max_profit) * 100
 
             # Calculate the mean of POP values
             mean_pop = pop_results.stack().mean()
@@ -216,15 +216,15 @@ def main():
             underlying_breakeven = short_strike + (short_price - long_price)
 
             # Calculate the probability to breakeven (POPBE)
-            popbe = calculate_popbe(percentage_to_cover_entry_cost, percentage_array, pop_results)
+            popbe = calculate_popbe(percentage_to_cover_entry_credit, percentage_array, pop_results)
 
             # Display the calculated values
-            st.write(f"Entry Cost: ${entry_cost:.2f}")
+            st.write(f"Entry Credit: ${entry_credit:.2f}")
             st.write(f"Maximum Risk: ${max_risk:.2f}")
             st.write(f"Maximum Return: ${max_profit:.2f}")
             st.write(f"Maximum Return on Risk: {max_return_on_risk * 100:.2f}%")
             st.write(f"Underlying Breakeven at Expiry: ${underlying_breakeven:.2f}")
-            st.write(f"Percentage to Cover Entry Cost: {percentage_to_cover_entry_cost:.2f}%")
+            st.write(f"Percentage to Cover Entry Credit: {percentage_to_cover_entry_credit:.2f}%")
             st.write(f"Probability to Breakeven at Expiry: {popbe:.2f}%")
             st.write(f"Arithmetic-Mean POP: {mean_pop:.2f}%")
             st.write(f"Geometric-Mean POP: {geometric_mean_pop * 100:.2f}%")

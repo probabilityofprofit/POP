409d88da05ca7437e938bf363964e1630d49405f
POPs <148493288+probabilityofprofit@users.noreply.github.com>
2023-10-26 14:37:01 -0700

    Update SLCallDebitSpread.py

--- pop/SLCallDebitSpread.py
+++ pop/SLCallDebitSpread.py
@@ -92,6 +92,7 @@ def custom_pop_colormap():
 
 # Streamlit UI
 def main():
+    try:
         st.title("Call Debit Spread")
 
         # Manual input of values
@@ -211,6 +212,10 @@ def main():
             st.write(f"Arithmetic-Mean POP: {mean_pop:.2f}%")
             st.write(f"Geometric-Mean POP: {geometric_mean_pop * 100:.2f}%")
             st.write(f"Probability of Profit: {probability_of_profit:.2f}%")
+
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 # Define a function to apply cell background color based on POP values
 def color_pop_cells(pop_value):
